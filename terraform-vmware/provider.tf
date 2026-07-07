82b92547358acb000e3b6768689431c1238216e1
Tn9h14 <t.nghia2605@gmail.com>
2026-07-08 02:53:34 +0700

    Fix: rewrite ansible

---
+++ terraform-vmware/provider.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    vcd = {
+      source  = "vmware/vcd"
+      version = "3.14.1"
+    }
+  }
+
+  backend "http" {
+    address        = "https://gitlab.nghia.internal/api/v4/projects/5/terraform/state/default"
+    lock_address   = "https://gitlab.nghia.internal/api/v4/projects/5/terraform/state/default/lock"
+    unlock_address = "https://gitlab.nghia.internal/api/v4/projects/5/terraform/state/default/lock"
+    lock_method    = "POST"
+    unlock_method  = "DELETE"
+    username       = "nghia"
+    password       = "glpat-6akM6t7KzeYXM_nMeJoS-m86MQp1OjMH.01.0w0y3w44c"
+  }
+}
+
+provider "vcd" {
+  user                 = var.vcd_user
+  password             = var.vcd_pass
+  auth_type            = "integrated"
+  org                  = var.vcd_org
+  vdc                  = var.vcd_vdc
+  url                  = var.vcd_url
+  max_retry_timeout    = var.vcd_max_retry_timeout
+  allow_unverified_ssl = var.vcd_allow_unverified_ssl
+}
